ca270474e0a75412141df7ac27ce5ccb597262b5
Harman <hdevs0001@gmail.com>
2026-09-17 17:42:04 +0530

    created the middleware for the app

---
+++ backend/src/middleware/error.middleware.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from "express";
+export function errorMiddleware(error: Error, req: Request, res: Response) {
+  console.error(error);
+  res.status(500).json({
+    success: false,
+    message: error.message || "Internal Server Error",
+  });
+}

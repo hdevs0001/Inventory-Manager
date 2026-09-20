660ff0a50404e80b0a4467d0f7218c460c2a2ddc
Harman <hdevs0001@gmail.com>
2026-09-20 22:25:51 +0530

    change the adapter form pg to neon and udpate the dockerfile because of some bug

--- backend/src/db/prisma.ts
+++ backend/src/db/prisma.ts
@@ -1,10 +1,20 @@
+// import "dotenv/config";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "../generated/prisma/client.js";
+
+// const connectionString = `${process.env.DATABASE_URL}`;
+
+// const adapter = new PrismaPg({ connectionString });
+// const prisma = new PrismaClient({ adapter });
+
+// export { prisma };
+
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL,
+});
 const prisma = new PrismaClient({ adapter });
-
-export { prisma }; 
+export { prisma };

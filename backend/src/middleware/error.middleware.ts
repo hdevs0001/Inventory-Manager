5bf8f557c3b9b2e0b21f08f3d34b6ed9b2953c29
Harman <hdevs0001@gmail.com>
2026-09-21 13:20:58 +0530

    changing the error.middleware.ts file in backend to see the error

--- backend/src/middleware/error.middleware.ts
+++ backend/src/middleware/error.middleware.ts
@@ -1,12 +1,30 @@
-import type { Request, Response, NextFunction } from "express";
+ import type { Request, Response, NextFunction } from "express";
 
+// export function errorMiddleware(
+//   error: Error,
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ): void {
+//   console.error(error);
+
+//   res.status(500).json({
+//     success: false,
+//     message: error.message || "Internal Server Error",
+//   });
+// }
 export function errorMiddleware(
   error: Error,
   req: Request,
   res: Response,
   next: NextFunction
-): void {
-  console.error(error);
+) {
+  console.error("===== ERROR =====");
+  console.error("Message:", error.message);
+  console.error("Name:", error.name);
+  console.error("Stack:", error.stack);
+  console.error("Full error:", error);
+  console.error("=================");
 
   res.status(500).json({
     success: false,
